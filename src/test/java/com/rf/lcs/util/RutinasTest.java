d49ef3d44bbbeee72952da88eb6ce21ea9525493
fprezur <fperezur@gmail.com>
2024-04-18 08:45:04 +0200

    RutinasTest.java, test unitarios

---
+++ src/test/java/com/rf/lcs/util/RutinasTest.java
@@ -0,0 +1,89 @@
+package com.rf.lcs.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RutinasTest {
+	
+	String STRING_NULA;
+	final String STRING_VACIA = "";
+	final String STRING_CON_DATOS = "Hola";
+	
+	final String CORREO_ELECTRONICO_CORRECTO = "migarcia@recursosformacion.com";
+	final String CORREO_ELECTRONICO_ERRONEO_1 = "migarcia.recursosformacion.com";
+	final String CORREO_ELECTRONICO_ERRONEO_2 = "migarcia@recursosformacion";
+	final String CORREO_ELECTRONICO_ERRONEO_3 = "@recursosformacion.com";
+	final String CORREO_ELECTRONICO_ERRONEO_4 = "migarcia@";
+	
+	@Test
+	void testIsEmpty() {
+		assertAll(
+				()-> assertTrue(Rutinas.isEmpty(STRING_NULA)),
+				()-> assertTrue(Rutinas.isEmpty(STRING_VACIA)),
+				()-> assertFalse(Rutinas.isEmpty(STRING_CON_DATOS))
+				);
+	}
+
+	@Test
+	void testIsEmailValid() {
+		assertAll(
+				()-> assertTrue(Rutinas.isEmailValid(CORREO_ELECTRONICO_CORRECTO)),
+				()-> assertFalse(Rutinas.isEmailValid(CORREO_ELECTRONICO_ERRONEO_1)),
+				()-> assertFalse(Rutinas.isEmailValid(CORREO_ELECTRONICO_ERRONEO_2)),
+				()-> assertFalse(Rutinas.isEmailValid(CORREO_ELECTRONICO_ERRONEO_3)),
+				()-> assertFalse(Rutinas.isEmailValid(CORREO_ELECTRONICO_ERRONEO_4))
+				);
+	}
+
+	@Test
+	void testNewIsNotEmpty() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsEmptyOrNull() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testValidDNI() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCompareDates() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDateIsGreater() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDateIsGreaterOrEqual() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDateIsLess() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDateIsLessOrEqual() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDateIsValid() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsPasswordValid() {
+		fail("Not yet implemented");
+	}
+
+}
