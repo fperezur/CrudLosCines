ad6182fa336ed2c086ba1cbec9ce9b211f0e14d0
fprezur <fperezur@gmail.com>
2024-04-19 13:04:53 +0200

    Añadidos métodos pruebas unitarias fechas y password

--- src/test/java/com/rf/lcs/util/RutinasTest.java
+++ src/test/java/com/rf/lcs/util/RutinasTest.java
@@ -1,19 +1,32 @@
 package com.rf.lcs.util;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class RutinasTest {
 	
+	//*************** VALIDACIONES DE STRING **********************************
 	String STRING_NULA;
 	final String STRING_VACIA = "";
 	final String STRING_CON_DATOS = "Hola";
 	final String STRING_CON_DATOS1 = "H";
 	
+	//*************** VALIDACIONES DE DNI **************************************
 	final String NUMERO_DNI_OK = "25.062.225-E";
 	final String NUMERO_DNI_ERROR_LETRA = "12.345.678-Ñ";
 	final String NUMERO_DNI_ERROR_FORM_CORTO = "12.2.678-A";
@@ -22,6 +35,7 @@ class RutinasTest {
 	final String NUMERO_DNI_ERROR_FORM_ERR = "12345678A";
 	final String NUMERO_DNI_ERROR_FORM_ERR2 = "12.345.678.A";
 	
+	//**************** VALIDACIONES DE EMAIL ************************************
 	final String CORREO_ELECTRONICO_CORRECTO = "migarcia@recursosformacion.com";
 	final String CORREO_ELECTRONICO_CORRECTO2 = "mighel.garcia@rf.com";
 	final String CORREO_ELECTRONICO_CORRECTO3 = "m@r.com";
@@ -30,6 +44,28 @@ class RutinasTest {
 	final String CORREO_ELECTRONICO_SIN_NOMBRE = "@recursosformacion.com";
 	final String CORREO_ELECTRONICO_SIN_DOMINIO = "migarcia@";
 	final String CORREO_ELECTRONICO_CON_NUMERO = "123@123.12";
+	
+	//************** VALIDACIONES DE FECHAS ********************************
+		final static LocalDate AHORA = LocalDate.now();
+		final static LocalDate MANIANA = LocalDate.now().plusDays(1);;
+		final static LocalDate AYER = LocalDate.now().minusDays(2);
+
+		final String FECHA_OK = "01/07/2022";
+		final String FECHA_OK_1 = "26/12/9999";
+		final String FECHA_OK_2 = "08/01/2022";
+		final String FECHA_OK_3 = "09/05/2021";
+
+		final String FECHA_ERROR = "2/30/2022";
+		final String FECHA_ERROR_1 = "2022/2/22";
+		final String FECHA_ERROR_2 = "31/2/2022";
+		final String FECHA_ERROR_3 = "2/9993/9";
+	
+		//***********Validaciones de contraseñas************
+		final String CONTRASENIA_OK = "qasLOASD#~@1!!!";
+		final String CONTRASENIA_ERROR = "12345";
+		final String CONTRASENIA_ERROR_1 = "123456789123456789123456789";
+		final String CONTRASENIA_ERROR_2 = "}}¬¬¬||SD";
+
 	
 	@ParameterizedTest
 	@NullAndEmptySource
@@ -87,10 +123,26 @@ class RutinasTest {
 	}
 
 	@Test
-	void testNewIsNotEmpty() {
-		fail("Not yet implemented");
+	void testNewIsNotEmptyStringString() {
+		String nulo = null;
+		String vacio = "";
+		String hola = "Hola";
+		String original = "original";
+		
+		assertAll(
+				() -> assertEquals(Rutinas.newIsNotEmptyStringString(original, nulo), "original"),
+				() -> assertEquals(Rutinas.newIsNotEmptyStringString(original, vacio), "original"),
+				() -> assertEquals(Rutinas.newIsNotEmptyStringString(original, hola), "Hola")
+				);
 	}
-
+	@ParameterizedTest
+	@CsvSource(value = {
+			"10,,10",
+			"7,25,25"
+	})
+	void testNewIsEmptyLongLong(Long tengo, Long llega, Long resultado) {
+		assertEquals(Rutinas.newIsNotEmptyLongLong(tengo, llega), resultado);
+	}
 	@Test
 	void testIsEmptyOrNull() {
 		fail("Not yet implemented");
@@ -114,10 +166,20 @@ class RutinasTest {
 		
 	}
 	
-
-	@Test
-	void testCompareDates() {
-		fail("Not yet implemented");
+	//INICIO TEST DE FECHAS
+	private static Stream<? extends Arguments> testDates(){
+		return Stream.of(
+					Arguments.of(List.of(AHORA, AHORA, 0)),
+					Arguments.of(List.of(AHORA, MANIANA, -1)),
+					Arguments.of(List.of(MANIANA, AHORA, 1)));
+	}
+	
+	@ParameterizedTest
+	@MethodSource("testDates")
+	void testCompareDates(List<LocalDate> variables) {
+		LocalDate fecha = (LocalDate) variables.get(0);
+		LocalDate referencia = (LocalDate) variables.get(1);
+		assertEquals(Rutinas.compareDates(fecha, referencia), variables.get(2));
 	}
 
 	@Test
@@ -145,9 +207,16 @@ class RutinasTest {
 		fail("Not yet implemented");
 	}
 
-	@Test
-	void testIsPasswordValid() {
-		fail("Not yet implemented");
+	@ParameterizedTest
+	@CsvSource(value = {
+			"true, qasLOASD#~@1!!!",
+			"false, adfgddsfasgE",
+			"false, adfgddsfasg$",
+			"false, adf"
+	})
+	void testIsPasswordValid(Boolean resultado, String cadena) {
+		assertEquals(Rutinas.isPasswordValid(cadena), resultado);
 	}
+	
 
 }
