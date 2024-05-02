3de325c26c6274ef89249122d14091ceece58c09
fprezur <fperezur@gmail.com>
2024-05-02 18:46:59 +0200

    Modificado método post y testeado todos los recursos en postman y funcionando

--- src/main/java/com/rf/lcs/controller/CineController.java
+++ src/main/java/com/rf/lcs/controller/CineController.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,7 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rf.lcs.exceptions.ControllerException;
@@ -28,9 +29,8 @@ import com.rf.lcs.servicio.CineService;
 
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(originPatterns = {"*"}, methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE })
 @RestController
 @RequestMapping("/api/cine")
 @Validated
@@ -39,91 +39,108 @@ public class CineController {
 	@Autowired
 	private CineService cineDao;
 
-	
-	@GetMapping("/{id}")
-	@Operation(description = "Recurso que consulta un cine por ID")
-	public ResponseEntity<Map<String, Object>> readOne(@PathVariable Long id){
-		Map<String, Object> map = new LinkedHashMap<>();
-		Optional<Cine> cine = cineDao.findById(id);
-		if(cine.isPresent()) {
-			map.put("status", 1);
-			map.put("data", cine.get());
-		} else {
-			map.put("status", 0);
-		}
+	@GetMapping("/test")//Comprobado en postman y funciona correctamente
+	public Cine getCineTest() {
+		Cine cine = null;
 		
-		return new ResponseEntity<>(map, HttpStatus.OK);
+		System.out.println("LLamando a getCineTest");
+		cine = new Cine(1L,"Albéniz", "Calle del centro, 8", "Centro", 50 );
+		
+		return cine;
 	}
-	
+	//Testeado en Postman y funciona, se obtiene los datos de un registro según su Id
+	@GetMapping("/{id}")
+	@Operation(description = "Servicio que consulta un cine por ID")
+	public ResponseEntity<Map<String, Object>> readOne(@PathVariable("id") Long ids) throws ControllerException{
+		String mensaje = "";
+		Map<String, Object> map = new LinkedHashMap<String, Object>();
+		if(ids != null) {
+			try {
+				Optional<Cine> cineDB = this.cineDao.findById(ids);
+				
+				if(cineDB.isPresent()) {
+					map.put("status", 1);
+					map.put("data", cineDB.get());
+					return new ResponseEntity<>(map, HttpStatus.OK);
+				} else {
+					mensaje = "NO existen datos";
+				}
+			} catch (Exception e) {
+				mensaje = "Formato erróneo";
+			}
+		}else {
+			mensaje = "Formato erróneo";
+		}
+		throw new ControllerException(mensaje);
+	}
+	//Testeado en Postman y funciona, se obtine listado de todos los registros
 	@GetMapping({"", "/"})
 	@Operation(description = "Recurso que lista todos los cines en la BBDD")
 	public ResponseEntity<Map<String, Object>> readAll() throws ControllerException{
-		Map<String, Object> map = new LinkedHashMap<>();
-		List<Cine> listaCines = cineDao.listAll();
+		Map<String, Object> map = new LinkedHashMap<String, Object>();
+		List<Cine> listaCines = this.cineDao.listAll();
 		if(!listaCines.isEmpty()) {
+			System.out.println(listaCines);
 			map.put("status", 1);
 			map.put("data", listaCines);
+			return new ResponseEntity<>(map, HttpStatus.OK);
 		} else {
 			throw new ControllerException("No hay registros para mostrar");
 		}
-		return new ResponseEntity<>(map, HttpStatus.OK);
 	}
-	
+	//Testeado en Postman y funciona
 	@PostMapping
 	@Operation(description = "Recurso que inserta un nuevo cine en la BBDD")
-	public ResponseEntity<Map<String, Object>> insertNew(@Valid @RequestBody Cine cineNew) 
+	public ResponseEntity<Map<String, Object>> insertNew(@Valid @RequestBody Cine cineNew, BindingResult bindingResult) 
 			throws DomainException, ControllerException, DaoException {
-		Map<String, Object> map = new LinkedHashMap<>();
-		
-		cineNew.setId_cine(0L);
-		try {
-			cineNew = cineDao.insert(cineNew);
-			map.put("status", 1);
-			map.put("data", cineNew);
-		} catch (Exception e) {
-			throw new ControllerException("Error al insertar el registro: " +
-					e.getMessage());
+		Map<String, Object> map = new LinkedHashMap<String, Object>();
+		if(!bindingResult.hasErrors()) {
+			cineNew.setId_cine(0L);
+			cineNew = this.cineDao.insert(cineNew);
+			if(cineNew != null) {
+				System.out.println("En alta: " + cineNew.toString());
+				map.put("status", 1);
+				map.put("data", cineNew);
+				return new ResponseEntity<>(map, HttpStatus.OK);
+			}else {
+				throw new ControllerException("Error al hacer la inserción");
+			}
 		}
-		
-		
-		return new ResponseEntity<>(map, HttpStatus.OK);
+		throw new ControllerException("Error de datos al hacer la inserción");
 	}
-	
+	//Testeado en Postman y funciona, modifica el registro existente
 	@PutMapping
 	@Operation(description = "Recurso que modifica un cine existente en la BBDD")
 	public ResponseEntity<Map<String, Object>> modificacion(@RequestBody Cine cineInput) throws ControllerException, DomainException, DaoException {
 		Map<String, Object> map = new LinkedHashMap<String, Object>();
 		if (cineDao.update(cineInput)) {
 			map.put("status", 1);
-			map.put("message", "Error al actualizar");
+			map.put("message", "Actualización correcta");
 			return new ResponseEntity<>(map, HttpStatus.OK);
 		} else {
 			throw new ControllerException("Error al hacer la modificacion");
 
 		}
 	}
-	
+	//Testeado en Postman y funciona, elimina registro existente según su Id
 	@DeleteMapping("/{id}")
 	@Operation(description = "Recurso que borra un cine en la BBDD")
-	public ResponseEntity<Map<String, Object>> deleteCine(@NotNull @PathVariable("id") Long id) 
-			throws DomainException, ControllerException, DaoException {
+	public ResponseEntity<Map<String, Object>> deleteCine(@PathVariable("id") String ids) 
+			throws ControllerException {
 		Map<String, Object> map = new LinkedHashMap<>();
-		try {
-			if(cineDao.deleteById(id)) {
-				map.put("status", 1);
-				map.put("data", "Registro borrado");
+		if(ids != null) {
+			try {
+					long id = Long.parseLong(ids);
+					Optional<Cine> cineDB = cineDao.findById(id);
+					cineDao.deleteById(cineDB.get().getId_cine());
+					map.put("status", 1);
+					map.put("data", "Registro borrado");
+					return new ResponseEntity<>(map, HttpStatus.OK);
+			} catch (Exception e) {
+				throw new ControllerException("Error al eliminar el registro: " +
+						e.getMessage());
 			}
-		} catch (Exception e) {
-			throw new ControllerException("Error al eliminar el registro: " +
-					e.getMessage());
 		}
-		
-		return new ResponseEntity<>(map, HttpStatus.OK);
-	}
-	
-	@RequestMapping("/test")
-	@Operation(description = "Recurso para comprobar el servicio")
-	public @ResponseBody String text() {
-		return "Test";
+		throw new ControllerException("No existe el registro a borrar");
 	}
 }
